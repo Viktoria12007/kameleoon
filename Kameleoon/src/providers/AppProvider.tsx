cc0c76d8089cce9828f78361499a6cd14175a6e6
Виктория Голубова <v.golubova@denexy.com>
2025-12-01 19:14:56 +0600

    Changed value for property 'theme'.

--- Kameleoon/src/providers/AppProvider.tsx
+++ Kameleoon/src/providers/AppProvider.tsx
@@ -7,7 +7,7 @@ export function AppProvider({ children }) {
         variation: { value: 1, label: "All variations selected" },
         timePeriod: { value: 0, label: "Day"},
         lineStyle: { value: 0, label: "Line style: line" },
-        theme: true,
+        theme: "light",
     });
 
     return (
--- Kameleoon/src/types/AppStateTypes.ts
+++ Kameleoon/src/types/AppStateTypes.ts
@@ -2,5 +2,5 @@ export default interface AppState {
     variation: { value: number, label: string },
     timePeriod: { value: number, label: string },
     lineStyle: { value: number, label: string },
-    theme: boolean,
+    theme: "light" | "dark",
 }
